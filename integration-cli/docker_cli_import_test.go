afb362372f316e1efd76146f33f9e69f0a631736
Gildas Cuisinier <gildas.cuisinier@gcuisinier.net>
2015-03-15 21:18:01 +0100

    allow import container from file://
    Signed-off-by: Gildas Cuisinier <gildas.cuisinier@gcuisinier.net>

--- integration-cli/docker_cli_import_test.go
+++ integration-cli/docker_cli_import_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"os/exec"
 	"strings"
 	"testing"
@@ -40,4 +41,50 @@ func TestImportDisplay(t *testing.T) {
 	}
 
 	logDone("import - display is fine, imported image runs")
+}
+
+func TestImportFile(t *testing.T) {
+	runCmd := exec.Command(dockerBinary, "run", "-d", "busybox", "true")
+	out, _, err := runCommandWithOutput(runCmd)
+	if err != nil {
+		t.Fatal("failed to create a container", out, err)
+	}
+	cleanedContainerID := stripTrailingCharacters(out)
+	defer deleteContainer(cleanedContainerID)
+
+	runCmd = exec.Command(dockerBinary, "export", cleanedContainerID)
+	out, _, err = runCommandWithOutput(runCmd)
+	if err != nil {
+		t.Fatal("failed to export a container", out, err)
+	}
+
+	temporaryFile, err := ioutil.TempFile("", "exportImportTest")
+	if err != nil {
+		t.Fatal("failed to create temporary file", "", err)
+	}
+	temporaryFile.WriteString(out)
+
+	runCmd = exec.Command(dockerBinary, "import", "file://"+temporaryFile.Name())
+	out, _, err = runCommandWithOutput(runCmd)
+	if err != nil {
+		t.Fatal("failed to import a container", out, err)
+	}
+
+	if n := strings.Count(out, "\n"); n != 1 {
+		t.Fatalf("display is messed up: %d '\\n' instead of 1:\n%s", n, out)
+	}
+	image := strings.TrimSpace(out)
+	defer deleteImages(image)
+
+	runCmd = exec.Command(dockerBinary, "run", "--rm", image, "true")
+	out, _, err = runCommandWithOutput(runCmd)
+	if err != nil {
+		t.Fatal("failed to create a container", out, err)
+	}
+
+	if out != "" {
+		t.Fatalf("command output should've been nothing, was %q", out)
+	}
+
+	logDone("import file:// is fine, imported image runs")
 }
